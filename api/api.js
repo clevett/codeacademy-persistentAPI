b9b2b2053c6611b02249cb3b90106aff1dc123dd
Cassie <cassielevett@gmail.com>
2020-04-19 17:37:05 -0500

    save api start, test not passing

--- api/api.js
+++ api/api.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const apiRouter = express.Router()
 
+const artistsRouter = require('./artists.js')
+
+apiRouter.use('/artists', artistsRouter)
+
 module.exports = apiRouter
--- api/artists.js
+++ api/artists.js
@@ -1,1 +1,17 @@
 const express = require('express')
+const artistsRouter = express.Router()
+
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
+
+artistsRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Artist WHERE Artist.is_currently_employed = 1', (error, artists) => {
+    if (error) {
+      next(error)
+    } else {
+      res.status(200).json({ artists })
+    }
+  })  
+})
+
+module.exports = artistsRouter
